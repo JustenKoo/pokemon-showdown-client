880379a5508e405e42e61767d88d2aa6fad56f83
Justen <jkoo545@mail.endicott.edu>
2026-08-07 16:24:31 -0400

    Remove Tournaments and Find a user from the home page

--- play.pokemonshowdown.com/src/panel-mainmenu.tsx
+++ play.pokemonshowdown.com/src/panel-mainmenu.tsx
@@ -748,12 +748,10 @@ class MainMenuPanel extends PSRoomPanel<MainMenuRoom> {
 					<div class="menugroup">
 						<p><a class="mainmenu2 mainmenu button" href="teambuilder">Teambuilder</a></p>
 						<p><a class={"mainmenu3 mainmenu" + onlineButton} href="ladder">Ladder</a></p>
-						<p><a class={"mainmenu4 mainmenu" + onlineButton} href="view-tournaments-all">Tournaments</a></p>
 					</div>
 
 					<div class="menugroup">
 						<p><a class={"mainmenu4 mainmenu" + onlineButton} href="battles">Watch a battle</a></p>
-						<p><a class={"mainmenu5 mainmenu" + onlineButton} href="users">Find a user</a></p>
 						<p><a class={"mainmenu6 mainmenu" + onlineButton} href="view-friends-all">Friends</a></p>
 						<p><a class={"mainmenu7 mainmenu" + onlineButton} href="resources">Info & Resources</a></p>
 					</div>
